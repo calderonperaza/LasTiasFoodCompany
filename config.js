d4990bc0d889d8b9ecfcde68f13f4a71546b9ce3
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2020-05-18 20:14:22 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 //archivo html, todo el codigo comun va aqui
 
 //Direccion URL de la api rest que se va a consumir por la aplicacion
+// importante no debe llevar pleca al final de la URL, ojo sin / al final
 const ApiRestUrl = "https://lastiasfoodbackend.us-south.cf.appdomain.cloud"
 
